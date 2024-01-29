e61cbfe7fd1971d21aebe029a8c696fa89eb6734
RPullia <robertopullia@gmail.com>
2024-01-29 15:57:41 +0100

    Added service and repository

---
+++ src/main/java/com/robp/serviceManager/domain/entity/ApplicationEntity.java
@@ -0,0 +1,39 @@
+package com.robp.serviceManager.domain.entity;
+
+import com.robp.serviceManager.enumeration.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "services")
+public class ServiceEntity {
+    @Id
+    @SequenceGenerator(
+            name = "service_id_sequence",
+            sequenceName = "service_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "service_id_sequence"
+    )
+    private Long id;
+
+    @NotEmpty(message = "IP address can't be empty")
+    private String ipAddress;
+    @NotEmpty(message = "Port number can't be empty")
+    @Column(unique = true)
+    private int portNumber;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Status status;
+
+}
