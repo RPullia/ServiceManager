be6743ac405175cb06f3e004fc8f84ccb6bbf895
RPullia <robertopullia@gmail.com>
2024-02-14 11:43:41 +0100

    Added CORS filter configuration

--- src/main/java/com/robp/serviceManager/ServiceManagerApplication.java
+++ src/main/java/com/robp/serviceManager/ServiceManagerApplication.java
@@ -3,10 +3,16 @@ package com.robp.serviceManager;
 import com.robp.serviceManager.domain.entity.ApplicationEntity;
 import com.robp.serviceManager.enumeration.Status;
 import com.robp.serviceManager.repository.ApplicationRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServiceManagerApplication {
@@ -21,4 +27,21 @@ public class ServiceManagerApplication {
 			applicationRepository.save(new ApplicationEntity(null, "192.168.1.27", 8088, "Test Service", "Test", "http://localhost:8090/application/image/leaf.png", Status.SERVICE_DOWN));
 		};
 	}*/
+
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.addAllowedOrigin("http://localhost:4200");
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin", "Accept,X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
